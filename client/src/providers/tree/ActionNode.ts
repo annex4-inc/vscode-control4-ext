f29f34677bf42a19a2e3b5665443ec573c902c47
Ryan Boucher <82889064+annex4@users.noreply.github.com>
2022-06-04 00:42:47 -0400

    Added 'getNameOfType' to ActionNode to fix webview issue for actions.

--- client/src/providers/tree/ActionNode.ts
+++ client/src/providers/tree/ActionNode.ts
@@ -2,12 +2,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { C4Action, C4Property } from '../../control4';
+import { C4Action } from '../../control4';
 import { TreeNode } from './TreeNode';
 
 export default class ActionNode extends TreeNode<C4Action> {
     constructor(name: string, action: C4Action) {
         super(name, action, "code", action.params && action.params.length == 0);
+    }
+
+    getNameOfType() {
+        return "C4Action"
     }
 
     contextValue = "action";
