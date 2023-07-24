5813b418b034e314873b6e31866ec294cce71e76
pmcgriff <pmcgriff@contexta3.com>
2023-07-24 13:02:01 -0500

    js files were excluded from git in gitignore

---
+++ client/src/svelte/pages/experienceicon.js
@@ -0,0 +1,8 @@
+// All files seem the same and property is used.
+import App from "../components/experienceicon.svelte";
+
+const app = new App({
+  target: document.body,
+});
+
+export default app;
