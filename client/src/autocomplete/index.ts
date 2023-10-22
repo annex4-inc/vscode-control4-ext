d4bc3663111bd1fee691896f767150ae4a378831
annex4 <82889064+annex4@users.noreply.github.com>
2023-10-21 20:13:47 -0400

    Initial untested changes for action type generation.

--- client/src/autocomplete/index.ts
+++ client/src/autocomplete/index.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import ActionsResource from '../components/actions';
 import CommandsResource from '../components/commands';
 import ConnectionsResource from '../components/connections';
@@ -5,7 +6,22 @@ import EventsResource from '../components/events';
 import PropertiesResource from '../components/properties';
 import ProxiesResource from '../components/proxies';
 import { C4Action } from '../control4/C4Action';
+import { WriteFileContents } from '../utility';
+import vscode from 'vscode';
 
-ActionsResource.emitter.on("changed", (items: C4Action[]) => {
+ActionsResource.emitter.on("changed", async (items: C4Action[]) => {
+    const workspacePath = vscode.workspace.workspaceFolders[0].uri.path;
+
+    let lines = ["---@meta", "", "---@class Hooks"];
+
+    items.forEach((action) => {
+        lines.push(`---@class Action.Params.${action.command}`)
+    })
+
+    items.forEach((action) => {
+        lines.push(`---@field onAction fun(action: "${action.command}" cb: fun(tParams: Action.Params.${action.command}))`)
+    })
+
+    let result = await WriteFileContents(path.join(workspacePath, "generated", "actions.type.lua"), lines.join("\r\n"));
     // TODO: Update a typings folder for hooks.onAction(<action>, <action_params>)
 })
