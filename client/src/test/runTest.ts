5240688c8c45d98daffc61ab1093d00020d74092
annex4 <82889064+annex4@users.noreply.github.com>
2024-08-21 00:01:28 -0400

    fix: bumped node version & fixed pathing for the build test.

--- client/src/test/runTest.ts
+++ client/src/test/runTest.ts
@@ -3,10 +3,12 @@ import { runTests } from '@vscode/test-electron';
 
 async function main() {
 	try {
-		const extensionDevelopmentPath = path.resolve(__dirname, '..', '..', '..');
+		const extensionDevelopmentPath = path.resolve(__dirname, process.platform === 'win32' ? '..' : '..', '..', '..');
 		const extensionTestsPath = path.resolve(__dirname, './suite');
-		const testWorkspace = path.resolve(__dirname, './fixtures/fixture1');
-		
+		const testWorkspace = path.resolve(__dirname, 'fixtures/fixture1');
+
+		console.log(extensionDevelopmentPath);
+
 		await runTests({ version: '1.92.0', extensionDevelopmentPath, extensionTestsPath, launchArgs: [ testWorkspace ] });
 	} catch (err) {
 		console.error('Failed to run tests');
